6feb1f6dc883561ef64f0cdb9ca7b40e34a63b16
Syah Muhammad Alfareizi <ofarisyeah@gmail.com>
2021-04-22 11:28:47 +0700

    final version + suhu

--- cobafaris.py
+++ cobafaris.py
@@ -1,21 +1,27 @@
-# final_version
+# final_version dengan sensor suhu
 import numpy as np
 import cv2
 import random
-
+from smbus2 import SMBus
+from mlx90614 import MLX90614
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 mouth_cascade = cv2.CascadeClassifier('haarcascade_mcs_mouth.xml')
 upper_body = cv2.CascadeClassifier('haarcascade_upperbody.xml')
 
+#sensor suhu
+bus = SMBus(1)
+sensor = MLX90614(bus, address=0x5A)
 
 # Adjust threshold value in range 80 to 105 based on your light.
 bw_threshold = 80
 
 # User message
 font = cv2.FONT_HERSHEY_SIMPLEX
-org = (30,  30)
+orgbaris1 = (30, 30)
+orgbaris2 = (30,50)
+orgbaris3 = (30,70)
 wfontnoface = (0,0,0)
 wfontwmask = (255, 255, 255)
 wfontnomask = (0, 0, 255)
@@ -25,6 +31,8 @@ maskerada = "Terima Kasih Telah Menggunakan Masker"
 maskerno = "Silahkan Pakai Masker dulu! "
 noface = "Wajah Tidak Ditemukan! "
 jumlahfps = "Frames per second : "
+suhubadan = "Suhu badan anda : "
+nilaisuhu = sensor.get_object_1()
 
 # Read video
 cap = cv2.VideoCapture(0)
@@ -52,10 +60,14 @@ while 1:
 
 
     if(len(faces) == 0 and len(faces_bw) == 0):
-        cv2.putText(img, noface + "| " + jumlahfps + format(fps), org, font, font_scale, wfontnoface, thickness, cv2.LINE_AA)
+        cv2.putText(img, noface , orgbaris1, font, font_scale, wfontnoface, thickness, cv2.LINE_AA)
+        cv2.putText(img,  jumlahfps + format(fps), orgbaris2, font, font_scale, wfontnoface, thickness, cv2.LINE_AA)
+        cv2.putText(img, suhubadan + str(nilaisuhu) , orgbaris3, font, font_scale, wfontnoface, thickness, cv2.LINE_AA)
     elif(len(faces) == 0 and len(faces_bw) == 1):
         # It has been observed that for white mask covering mouth, with gray image face prediction is not happening
-        cv2.putText(img, maskerada + "| " + jumlahfps + format(fps), org, font, font_scale, wfontwmask, thickness, cv2.LINE_AA)
+        cv2.putText(img, maskerada , orgbaris1, font, font_scale, wfontwmask, thickness, cv2.LINE_AA)
+        cv2.putText(img, jumlahfps + format(fps), orgbaris2, font, font_scale, wfontwmask, thickness, cv2.LINE_AA)
+        cv2.putText(img, suhubadan + str(nilaisuhu), orgbaris3, font, font_scale, wfontwmask, thickness, cv2.LINE_AA)
     else:
         # Draw rectangle on gace
         for (x, y, w, h) in faces:
@@ -66,21 +78,28 @@ while 1:
             mouth_rects = mouth_cascade.detectMultiScale(gray, 1.5, 5)
         # Face detected but Lips not detected which means person is wearing mask
         if(len(mouth_rects) == 0 ):
-            cv2.putText(img, maskerada + "| " + jumlahfps + format(fps), org, font, font_scale, wfontwmask, thickness, cv2.LINE_AA)
+            cv2.putText(img, maskerada , orgbaris1, font, font_scale, wfontwmask, thickness, cv2.LINE_AA)
+            cv2.putText(img, jumlahfps + format(fps), orgbaris2, font, font_scale, wfontwmask, thickness, cv2.LINE_AA)
+            cv2.putText(img, suhubadan + str(nilaisuhu), orgbaris3, font, font_scale, wfontwmask, thickness, cv2.LINE_AA)
         else:
             for (mx, my, mw, mh) in mouth_rects:
                 if(y < my < y + h):
                     # Face and Lips are detected but lips coordinates are within face cordinates which `means lips prediction is true and
                     # person is not waring mask
-                    cv2.putText(img, maskerno + "| " + jumlahfps + format(fps), org, font, font_scale, wfontnomask, thickness, cv2.LINE_AA)
+                    cv2.putText(img, maskerno , orgbaris1, font, font_scale, wfontnomask, thickness, cv2.LINE_AA)
+                    cv2.putText(img, jumlahfps + format(fps), orgbaris2, font, font_scale, wfontnomask, thickness, cv2.LINE_AA)
+                    cv2.putText(img, suhubadan + str(nilaisuhu), orgbaris3, font, font_scale, wfontnomask, thickness, cv2.LINE_AA)
                     cv2.rectangle(img, (mx, my), (mx + mh, my + mw), (255, 0, 0), 3)
                     break
+                    
+    
 
 
     # Show frame with results
     cv2.imshow('Mask Detection', img)
     k = cv2.waitKey(30) & 0xff
     if k == ord('q'):
+        bus.close()
         break
 
 # Release video
