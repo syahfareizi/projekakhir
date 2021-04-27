7cd74cc82d1152589133a7ccc46b04ba9bb211b4
Syah Muhammad Alfareizi <ofarisyeah@gmail.com>
2021-04-28 01:26:38 +0700

    tanpa suhu + servo ( Metode Video )

--- 1.0.1.py
+++ 1.0.1.py
@@ -9,6 +9,7 @@ import time
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 mouth_cascade = cv2.CascadeClassifier('haarcascade_mcs_nose.xml')
+mask_cascade = cv2.CascadeClassifier ('cascade.xml')
 
 #sensor suhu
 #bus = SMBus(1)
@@ -45,40 +46,33 @@ def ambilgambar():
     
 def nofaces():
     cv2.putText(img, noface , orgbaris1, font, font_scale, wfontnoface, thickness, cv2.LINE_AA)
-    cv2.putText(img,  jumlahfps + format(fps), orgbaris2, font, font_scale, wfontnoface, thickness, cv2.LINE_AA)
     #cv2.putText(img, suhubadan + str(nilaisuhu) , orgbaris3, font, font_scale, wfontnoface, thickness, cv2.LINE_AA)
     #cv2.putText(img, pesanakhir1 , orgbaris4, font, font_scale, wfontnomask, thickness, cv2.LINE_AA)
 
 def maskernosuhu():
     cv2.putText(img, maskerada , orgbaris1, font, font_scale, wfontwmask, thickness, cv2.LINE_AA)
-    cv2.putText(img, jumlahfps + format(fps), orgbaris2, font, font_scale, wfontwmask, thickness, cv2.LINE_AA)
     #cv2.putText(img, suhubadan + str(nilaisuhu), orgbaris3, font, font_scale, wfontnowmask, thickness, cv2.LINE_AA)
     #cv2.putText(img, pesanakhir1 , orgbaris4, font, font_scale, wfontnomask, thickness, cv2.LINE_AA)
 
 def maskersuhu():
     cv2.putText(img, maskerada , orgbaris1, font, font_scale, wfontwmask, thickness, cv2.LINE_AA)
-    cv2.putText(img, jumlahfps + format(fps), orgbaris2, font, font_scale, wfontwmask, thickness, cv2.LINE_AA)
     #cv2.putText(img, suhubadan + str(nilaisuhu), orgbaris3, font, font_scale, wfontwmask, thickness, cv2.LINE_AA)
     #cv2.putText(img, pesanakhir2 , orgbaris4, font, font_scale, wfontnomask, thickness, cv2.LINE_AA)
     #time.sleep(5)
 
 def nomaskernosuhu():
     cv2.putText(img, maskerno , orgbaris1, font, font_scale, wfontnomask, thickness, cv2.LINE_AA)
-    cv2.putText(img, jumlahfps + format(fps), orgbaris2, font, font_scale, wfontwmask, thickness, cv2.LINE_AA)
     #cv2.putText(img, suhubadan + str(nilaisuhu), orgbaris3, font, font_scale, wfontnomask, thickness, cv2.LINE_AA)
     #cv2.putText(img, pesanakhir1 , orgbaris4, font, font_scale, wfontnomask, thickness, cv2.LINE_AA)
     
 # membaca video
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
 # fps
 waktusebelum = 0
 waktusesudah = 0
     
-while 1:
-    # FPS COUNTER
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    
+while 1:    
     #nilai suhu
     #nilaisuhu = sensor.get_object_1()
     
@@ -115,17 +109,25 @@ while 1:
             roi_color = img[y:y + h, x:x + w]
             # Detect lips counters
             mouth_rects = mouth_cascade.detectMultiScale(gray, 1.5, 5)
-        # Face detected but Lips not detected which means person is wearing mask
-            if(len(mouth_rects) == 0  ):
-                maskernosuhu()
-            if(len(mouth_rects) == 0 ):
-                maskersuhu()
+            adamasker = mask_cascade.detectMultiScale(gray, 1.1, 4 )
+            # Face detected but Lips not detected which means person is wearing mask
+            if(len(mouth_rects) == 0 and len(adamasker)==1 ):
+                for (kx, ky, kw, kh) in adamasker :
+                    cv2.rectangle(img, (kx, ky), (kx + kh, ky + kw), (255, 0, 255), 3)
+                    maskersuhu()
+            elif(len(mouth_rects) == 0 and len(adamasker)==0):
+                nomaskernosuhu()
+            elif(len(mouth_rects) == 1 and len(adamasker)== 1):
+                for (mx, my, mw, mh) in mouth_rects:
+                    cv2.rectangle(img, (mx, my), (mx + mh, my + mw), (255, 0, 0), 3)
+                    nomaskernosuhu()
+                for (kx, ky, kw, kh) in adamasker :
+                    cv2.rectangle(img, (kx, ky), (kx + kh, ky + kw), (255, 0, 255), 3)
             else:
                 for (mx, my, mw, mh) in mouth_rects:
-                    if(y < my < y + h):
-                        cv2.rectangle(img, (mx, my), (mx + mh, my + mw), (255, 0, 0), 3)
-                        nomaskernosuhu()
-                        break
+                    cv2.rectangle(img, (mx, my), (mx + mh, my + mw), (255, 0, 0), 3)
+                    nomaskernosuhu()
+                    break
     
     waktusesudah = time.time()
     
@@ -138,7 +140,7 @@ while 1:
     # converting the fps to string so that we can display it on frame
     # by using putText function
     fps = str(fps)
-    cv2.putText(img, fps , orgbaris5, font, font_scale, wfontwmask, thickness, cv2.LINE_AA)
+    cv2.putText(img,  jumlahfps + format(fps), orgbaris2, font, font_scale, wfontnoface, thickness, cv2.LINE_AA)
                         
     # Show frame with results
     cv2.imshow('Mask Detection', img)
